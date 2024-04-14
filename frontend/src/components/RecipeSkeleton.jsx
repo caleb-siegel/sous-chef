8e74abfdeed257c171b168df78a6bb8cddf2d6d3
Caleb Siegel <caleb.siegel@gmail.com>
2024-04-14 11:33:55 -0400

    add skeleton

---
+++ frontend/src/components/RecipeSkeleton.jsx
@@ -0,0 +1,43 @@
+import React from 'react'
+import { Stack, Skeleton, Box } from '@mui/material'
+
+function RecipeSkeleton() {
+  return (
+    <Box display="flex" flexDirection="row" flexWrap="wrap">
+        <Box>
+            <Stack spacing={1}>
+                <Skeleton variant="circular" width={40} height={40} />
+                <Skeleton variant="rectangular" width={210} height={200} />
+                <Skeleton variant="rounded" width={210} height={30} />
+                <Skeleton variant="rounded" width={210} height={30} />
+            </Stack>
+        </Box>
+        <Box>
+            <Stack spacing={1} ml={4}>
+                <Skeleton variant="circular" width={40} height={40} />
+                <Skeleton variant="rectangular" width={210} height={200} />
+                <Skeleton variant="rounded" width={210} height={30} />
+                <Skeleton variant="rounded" width={210} height={30} />
+            </Stack>
+        </Box>
+        <Box>
+            <Stack spacing={1} ml={4}>
+                <Skeleton variant="circular" width={40} height={40} />
+                <Skeleton variant="rectangular" width={210} height={200} />
+                <Skeleton variant="rounded" width={210} height={30} />
+                <Skeleton variant="rounded" width={210} height={30} />
+            </Stack>
+        </Box>
+        <Box>
+            <Stack spacing={1} ml={4}>
+                <Skeleton variant="circular" width={40} height={40} />
+                <Skeleton variant="rectangular" width={210} height={200} />
+                <Skeleton variant="rounded" width={210} height={30} />
+                <Skeleton variant="rounded" width={210} height={30} />
+            </Stack>
+        </Box>
+    </Box>
+  )
+}
+
+export default RecipeSkeleton
