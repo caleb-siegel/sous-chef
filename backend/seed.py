4fea646ff4c3a17a663b5f281fb0af28aba77c34
Caleb Siegel <caleb.siegel@gmail.com>
2024-09-06 07:16:11 -0400

    add more recipe images

--- backend/seed.py
+++ backend/seed.py
@@ -57,60 +57,172 @@ with app.app_context():
     # db.session.commit()
 
 
-    # Recipe: White Chocolate Chunk Cookies
-    white_chocolate_chunk_cookies_recipe = Recipe(
-        name="White Chocolate Chunk Cookies",
-        picture="/recipes/white_chocolate_chunk_cookies.jpeg",
+    # Recipe 1: General Tso's Chicken and Broccoli Bake
+    general_tsos_chicken_and_broccoli_bake_recipe = Recipe(
+        name="General Tso's Chicken and Broccoli Bake",
+        picture="/recipes/general_tsos_chicken_and_broccoli_bake.jpeg",
         source_category_id=2,
-        source="Marblespoon at Home",
-        reference="298",
+        source="Simply Gourmet",
+        reference="168",
         instructions="""
     Step 1
-    Preheat oven to 350°F. Line 2 baking sheets with parchment paper; set aside.
+    Preheat oven to 350°F. Coat a 9x13-inch baking pan with cooking spray.
 
     Step 2
-    In a medium mixing bowl, combine flour, baking powder, and baking soda. Set aside.
+    Place chicken strips, flour, salt, and pepper into a resealable bag. Seal bag; shake until all chicken strips are well coated. Remove strips from flour. Shake off the excess flour; place chicken strips into prepared baking pan.
 
     Step 3
-    To a second mixing bowl, add butter. Using a hand mixer, beat for 1 minute.
+    Prepare the General Tso's sauce: In a small saucepan, whisk together sugar and cornstarch. Whisk in water. Add remaining ingredients; whisk until smooth. Bring to a boil over medium-high heat, stirring constantly. Lower heat to a simmer and allow sauce to thicken until it lightly coats the back of a spoon. Remove from heat. Alternatively, place mixture into a microwave-safe bowl or container. Microwave on high for 1 minute; stir well. Repeat this two more times for a total of 3 minutes in the microwave. Mixture will be smooth and thickened.
 
     Step 4
-    Add sugar and brown sugar; continue beating until creamy.
+    Pour half the sauce over the chicken strips. Cover pan with foil; bake for 30 minutes.
 
     Step 5
-    Add egg and vanilla; beat until incorporated. Stir in dry ingredients until well incorporated.
+    Remove pan from oven. Place broccoli florets over the chicken strips. Pour remaining sauce over the broccoli. Cover; bake an additional 15-20 minutes.
 
     Step 6
-    Stir most of the white chocolate pieces into the dough, reserving some for the top. Mix well. Cover bowl; chill for 30 minutes.
-
-    Step 7
-    Using an ice cream scoop, scoop 12-15 balls of dough onto prepared baking sheets, spaced about 2 inches apart. Slightly flatten each ball. Top with reserved white chocolate pieces.
-
-    Step 8
-    Bake for 8-10 minutes, or until the edges are slightly golden but cookies are still soft. They will continue to set as they cool.
+    Before serving, sprinkle with glazed cashews and sesame seeds, if using.
     """
     )
 
-    db.session.add(white_chocolate_chunk_cookies_recipe)
+    db.session.add(general_tsos_chicken_and_broccoli_bake_recipe)
     db.session.commit()
 
-    white_chocolate_chunk_cookies_recipe_id = white_chocolate_chunk_cookies_recipe.id
+    general_tsos_chicken_and_broccoli_bake_recipe_id = general_tsos_chicken_and_broccoli_bake_recipe.id
 
-    white_chocolate_chunk_cookies_ingredients = [
-        {"name": "All-purpose flour", "quantity": 1.5, "unit": "cups", "note": ""},
-        {"name": "Baking powder", "quantity": 0.5, "unit": "tsp", "note": ""},
-        {"name": "Baking soda", "quantity": 0.5, "unit": "tsp", "note": ""},
-        {"name": "Butter, at room temperature", "quantity": 7, "unit": "Tbsp", "note": ""},
-        {"name": "Sugar", "quantity": 0.5, "unit": "cup", "note": ""},
-        {"name": "Brown sugar", "quantity": 0.25, "unit": "cup", "note": ""},
-        {"name": "Egg", "quantity": 1, "unit": "", "note": ""},
-        {"name": "Pure vanilla extract", "quantity": 1, "unit": "Tbsp", "note": ""},
-        {"name": "White chocolate bars, roughly chopped", "quantity": 7, "unit": "oz", "note": "Two 3.5-oz bars"}
+    general_tsos_chicken_and_broccoli_bake_ingredients = [
+        {"name": "Chicken cutlets, cut into strips", "quantity": 1.5, "unit": "lb", "note": ""},
+        {"name": "Flour", "quantity": 0.25, "unit": "cup", "note": ""},
+        {"name": "Kosher salt", "quantity": 1, "unit": "tsp", "note": ""},
+        {"name": "Black pepper", "quantity": 0.5, "unit": "tsp", "note": ""},
+        {"name": "Frozen broccoli florets, defrosted", "quantity": 24, "unit": "oz", "note": ""},
+        {"name": "Honey-glazed cashews", "quantity": 0.5, "unit": "cup", "note": "Optional"},
+        {"name": "Black and white sesame seeds", "quantity": 0, "unit": "", "note": "Optional, for sprinkling"},
+        {"name": "Sugar", "quantity": 0.5, "unit": "cup", "note": "General Tso's Sauce"},
+        {"name": "Cornstarch", "quantity": 2, "unit": "Tbsp", "note": "General Tso's Sauce"},
+        {"name": "Water", "quantity": 0.5, "unit": "cup", "note": "General Tso's Sauce"},
+        {"name": "Rice vinegar", "quantity": 0.5, "unit": "cup", "note": "General Tso's Sauce"},
+        {"name": "Low sodium soy sauce", "quantity": 0.5, "unit": "cup", "note": "General Tso's Sauce"},
+        {"name": "Hoisin sauce", "quantity": 1.5, "unit": "Tbsp", "note": "General Tso's Sauce"},
+        {"name": "Garlic, crushed", "quantity": 4, "unit": "cloves", "note": "General Tso's Sauce"},
+        {"name": "Ground ginger", "quantity": 0.5, "unit": "tsp", "note": "General Tso's Sauce"},
+        {"name": "Crushed red pepper flakes", "quantity": 0.25, "unit": "tsp", "note": "General Tso's Sauce"}
     ]
 
-    for ing in white_chocolate_chunk_cookies_ingredients:
+    for ing in general_tsos_chicken_and_broccoli_bake_ingredients:
         ingredient_entry = Recipe_Ingredient(
-            recipe_id=white_chocolate_chunk_cookies_recipe_id,
+            recipe_id=general_tsos_chicken_and_broccoli_bake_recipe_id,
+            ingredient_name=ing["name"],
+            ingredient_quantity=ing["quantity"],
+            ingredient_unit=ing["unit"],
+            ingredient_note=ing["note"]
+        )
+        db.session.add(ingredient_entry)
+
+    # Recipe 2: Sweet Chili-Glazed Chicken Wings
+    sweet_chili_glazed_chicken_wings_recipe = Recipe(
+        name="Sweet Chili-Glazed Chicken Wings",
+        picture="/recipes/sweet_chili_glazed_chicken_wings.jpeg",
+        source_category_id=2,
+        source="Simply Gourmet",
+        reference="158",
+        instructions="""
+    Step 1
+    Preheat oven to 450°F. Set out a baking sheet.
+
+    Step 2
+    Pat wings dry with a paper towel (this will help crisp the skin). Place wings on prepared baking sheet. Drizzle with olive oil; sprinkle with salt and pepper. Turn wings to coat evenly.
+
+    Step 3
+    Bake skin-side up, uncovered, for 20 minutes.
+
+    Step 4
+    Prepare the sweet chili glaze: In a small bowl, combine glaze ingredients.
+
+    Step 5
+    Remove wings from oven. Pour off any liquid. Turn wings over. Pour glaze over wings. Bake for 15 minutes. Brush wings with glaze from the baking sheet. Bake for another 15 minutes.
+
+    Step 6
+    Turn wings skin-side up. Brush wings once again with glaze. Raise oven temperature to 500°F. Bake an additional 10-15 minutes. Wings will have a fabulous BBQ look.
+    """
+    )
+
+    db.session.add(sweet_chili_glazed_chicken_wings_recipe)
+    db.session.commit()
+
+    sweet_chili_glazed_chicken_wings_recipe_id = sweet_chili_glazed_chicken_wings_recipe.id
+
+    sweet_chili_glazed_chicken_wings_ingredients = [
+        {"name": "Chicken wings", "quantity": 2, "unit": "lb", "note": ""},
+        {"name": "Olive oil", "quantity": 3, "unit": "Tbsp", "note": ""},
+        {"name": "Kosher salt", "quantity": 1, "unit": "Tbsp", "note": ""},
+        {"name": "Black pepper", "quantity": 0.5, "unit": "tsp", "note": ""},
+        {"name": "Honey", "quantity": 0.5, "unit": "cup", "note": "Sweet Chili Glaze"},
+        {"name": "Sweet chili sauce", "quantity": 0.5, "unit": "cup", "note": "Sweet Chili Glaze"},
+        {"name": "Garlic, crushed", "quantity": 5, "unit": "cloves", "note": "Sweet Chili Glaze"},
+        {"name": "Low sodium soy sauce", "quantity": 3, "unit": "Tbsp", "note": "Sweet Chili Glaze"},
+        {"name": "Apple cider vinegar", "quantity": 3, "unit": "Tbsp", "note": "Sweet Chili Glaze"},
+        {"name": "Crushed red pepper flakes", "quantity": 0.25, "unit": "tsp", "note": "Sweet Chili Glaze"}
+    ]
+
+    for ing in sweet_chili_glazed_chicken_wings_ingredients:
+        ingredient_entry = Recipe_Ingredient(
+            recipe_id=sweet_chili_glazed_chicken_wings_recipe_id,
+            ingredient_name=ing["name"],
+            ingredient_quantity=ing["quantity"],
+            ingredient_unit=ing["unit"],
+            ingredient_note=ing["note"]
+        )
+        db.session.add(ingredient_entry)
+
+    # Recipe 3: Savory Herb-Rubbed Chicken
+    savory_herb_rubbed_chicken_recipe = Recipe(
+        name="Savory Herb-Rubbed Chicken",
+        picture="/recipes/savory_herb_rubbed_chicken.jpeg",
+        source_category_id=2,
+        source="Simply Gourmet",
+        reference="156",
+        instructions="""
+    Step 1 (Optional)
+    Brine the chicken: Fill a large container with brining liquid ingredients. Mix all together. Add chicken; marinate, covered, in refrigerator for at least one hour or overnight.
+
+    Step 2
+    Prepare the savory herb rub: In a small bowl, mix together rub ingredients.
+
+    Step 3
+    Preheat oven to 350°F. Prepare a 9x13-inch baking pan.
+
+    Step 4
+    Remove chicken from brine, if using. Pat chicken dry; coat generously with rub. Cover; bake for 1 hour 15 minutes. Uncover, drizzle with honey. Broil for five minutes.
+    """
+    )
+
+    db.session.add(savory_herb_rubbed_chicken_recipe)
+    db.session.commit()
+
+    savory_herb_rubbed_chicken_recipe_id = savory_herb_rubbed_chicken_recipe.id
+
+    savory_herb_rubbed_chicken_ingredients = [
+        {"name": "Chickens, cut into quarters or eighths", "quantity": 6, "unit": "lb", "note": ""},
+        {"name": "Honey", "quantity": 3, "unit": "Tbsp", "note": ""},
+        {"name": "Dark brown sugar (packed)", "quantity": 9, "unit": "Tbsp", "note": "Savory Herb Rub"},
+        {"name": "Sweet or smoked paprika", "quantity": 6, "unit": "Tbsp", "note": "Savory Herb Rub"},
+        {"name": "Chili powder", "quantity": 4.5, "unit": "Tbsp", "note": "Savory Herb Rub"},
+        {"name": "Kosher salt", "quantity": 3, "unit": "tsp", "note": "Savory Herb Rub"},
+        {"name": "Garlic powder", "quantity": 3, "unit": "tsp", "note": "Savory Herb Rub"},
+        {"name": "Black pepper", "quantity": 1.5, "unit": "tsp", "note": "Savory Herb Rub"},
+        {"name": "Cumin", "quantity": 1.5, "unit": "tsp", "note": "Savory Herb Rub"},
+        {"name": "Crushed red pepper flakes", "quantity": 1, "unit": "tsp", "note": "Savory Herb Rub"},
+        {"name": "Nutmeg", "quantity": 0.5, "unit": "tsp", "note": "Savory Herb Rub"},
+        {"name": "Water", "quantity": 4, "unit": "cups", "note": "Optional Brining Liquid"},
+        {"name": "Dark brown sugar", "quantity": 0.5, "unit": "cup", "note": "Optional Brining Liquid"},
+        {"name": "Kosher salt", "quantity": 0.5, "unit": "cup", "note": "Optional Brining Liquid"},
+        {"name": "Vinegar", "quantity": 0.5, "unit": "cup", "note": "Optional Brining Liquid"}
+    ]
+
+    for ing in savory_herb_rubbed_chicken_ingredients:
+        ingredient_entry = Recipe_Ingredient(
+            recipe_id=savory_herb_rubbed_chicken_recipe_id,
             ingredient_name=ing["name"],
             ingredient_quantity=ing["quantity"],
             ingredient_unit=ing["unit"],
