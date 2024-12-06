8c6d5c3f7f0ab8937d21e47dabe8c0cc8a08f88f
Caleb Siegel <caleb.siegel@gmail.com>
2024-12-06 07:36:51 -0500

    refactor recipe route

--- backend/app.py
+++ backend/app.py
@@ -523,5 +523,74 @@ def user_tag_names():
 
     return response
 
+@app.route('/api/recipe_info')
+def recipe_info():
+    recipes = []
+    for recipe in Recipe.query.order_by(Recipe.id.desc()).all():
+        # make meal prep list
+        meal_preps = []
+        for meal_prep in recipe.meal_preps:
+            prep = {
+                "id": meal_prep.id,
+                "meal": meal_prep.meal,
+                "recipe_id": meal_prep.recipe_id,
+                "user_id": meal_prep.user_id,
+                "weekday": meal_prep.weekday,
+            }
+            meal_preps.append(prep)
+
+        # make recipe_tag list
+        recipe_tags = []
+        for tag in recipe.recipe_tags:
+            # make tag object
+            tag_dict = {
+                "id": tag.tag.id,
+                "name": tag.tag.name
+            }
+
+            recipe_tag = {
+                "id": tag.id,
+                "recipe_id": tag.recipe_id,
+                "tag_id": tag.tag_id,
+                "tag": tag_dict,
+            }
+            recipe_tags.append(recipe_tag)
+
+        # make user recipe tag list
+        user_recipe_tags = []
+        for tag in recipe.user_recipe_tags:
+            # make user_tag object
+            user_tag_dict = {
+                "id": tag.user_tag.id,
+                "name": tag.user_tag.name
+            }
+
+            user_recipe_tag = {
+                "id": tag.id,
+                "user_id": tag.user_id,
+                "recipe_id": tag.recipe_id,
+                "user_tag_id": tag.user_tag_id,
+                "user_tag": user_tag_dict,
+                # "user": tag.user,
+            }
+            user_recipe_tags.append(user_recipe_tag)
+        
+        recipe_dict = {
+            "id": recipe.id,
+            "name": recipe.name,
+            "picture": recipe.picture,
+            "meal_preps": meal_preps,
+            "recipe_tags": recipe_tags,
+            "user_recipe_tags": user_recipe_tags,
+        }
+        recipes.append(recipe_dict)
+
+    response = make_response(
+        recipes,
+        200
+    )
+
+    return response
+
 if __name__ == "__main__":
     app.run(port=5555, debug=True)
